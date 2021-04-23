1a71ba829df895b297da769aa340ccf7be33255b
wwy2178 <994292178@qq.com>
2021-04-23 19:53:05 +0800

    初次提交

---
+++ app/src/main/java/com/audioto/mygitglide/MainActivity.java
@@ -0,0 +1,17 @@
+package com.audioto.mygitglide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.charger.toast_wwy.WwyTest;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ float_image_text/src/main/java/com/float_image/text/widget/TestJava.java
@@ -0,0 +1,9 @@
+package com.float_image.text.widget;
+
+/**
+ * Auther: Created by Admin on 2021/4/23
+ * Description :
+ */
+public class TestJava {
+    //
+}
